4ddb3d81452165fc89298040addde66858bdf7ef
moulithar <moulipri@gmail.com>
2023-04-29 11:41:13 +0530

    carousel update

--- src/components/Owl/Owl.js
+++ src/components/Owl/Owl.js
@@ -12,11 +12,12 @@ const Owl= ()=>
 {
     var settings = {
         dots: true,
-        infinite: true,
-        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 2000,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
       };
+
     return(
         <>
         <section className="slider-banner">
@@ -39,6 +40,36 @@ const Owl= ()=>
                     <h4>women fashion</h4>
                     </div>
                 </div>
+                <div>
+                    <img src={bannerimg} />
+                    <div className="content">
+                    <h2>welcome to fashion</h2>
+                    <h4>men fashion</h4>
+                  
+                    </div>
+                </div>
+                <div>
+                    <img src={bannersecond} />
+                    <div className="content">
+                   <h2> welcome to fashion</h2>
+                    <h4>women fashion</h4>
+                    </div>
+                </div>
+                <div>
+                    {/* <img src={bannerimg} />
+                    <div className="content">
+                    <h2>welcome </h2>
+                    <h4>fashion</h4>
+                  
+                    </div>
+                </div>
+                <div>
+                    <img src={bannersecond} />
+                    <div className="content">
+                   <h2> Qertrtycome four</h2>
+                    <h4>women dreamon</h4>
+            </div> */}
+                </div> 
             </Slider>
         </section>
         </>
