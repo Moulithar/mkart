83733756a6d6d582109f410c74b08812adcf8894
Mouli <128670409+Moulithar@users.noreply.github.com>
2024-03-09 15:22:06 +0530

    dna  logo styupid commit

--- src/components/Offer/Offer.js
+++ src/components/Offer/Offer.js
@@ -3,7 +3,7 @@ import React from "react"
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
-import ""
+import dna_image from "../../../public/static/dna_logo.png"
 // import Form from 'react-bootstrap/Form';
 // import logo from "./assets/styles/logo.png";
 
@@ -12,14 +12,15 @@ const Offer = () => {
     <div className="offer text-center py-5">
       <Container>
         <Row>
+          <img src={dna_image} alt="dna_image" className="img-fluid" style={{width: "10px"}}/>
           <span>
-            <h6> special offer</h6>
+            <h6> special offer 3 </h6>
           </span>
           <h3>top collection</h3>
 
           <p className="text-center mb-4">
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. yeah also abigail been streak commit 2.
+            industry. yeah also abigail been streak commit 3.
             <br />
             Lorem Ipsum has been the industry's standard dummy text ever since
             the 1500s,
