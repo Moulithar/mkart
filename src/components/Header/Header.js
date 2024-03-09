6cdd593d5cefe18e6fbd1b8ac51551209385a60b
Mouli <128670409+Moulithar@users.noreply.github.com>
2024-03-09 16:07:48 +0530

    img removed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar"
 // import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from "./assets/images/logo.png"
-import dna from "./assets/images/dna_logo.png"
+import dna from "../../../public/static/dna_logo.png"
 import "./assets/styles/index.css"
 import { Offcanvas } from "react-bootstrap"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
@@ -108,15 +108,7 @@ const Header = () => {
                       height: "auto",
                     }}
                   />
-                  <img
-                    src={dna}
-                    className="logo"
-                    alt=""
-                    style={{
-                      width: "100px",
-                      height: "auto",
-                    }}
-                  />
+             
                 </Navbar.Brand>
               </Link>
               {/* <Navbar.Toggle aria-controls="navbarScroll" /> */}
